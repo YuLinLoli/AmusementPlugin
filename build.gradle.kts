70028fc96db88d36fa7cac7543d381d5af08ed4b
cheng_ying@outlook.com <cheng_ying@outlook.com>
2023-07-23 17:37:54 +0800

    1.1.1-test 添加了黑名单功能，优化了部分代码结构！ 修复了部分bug

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     kotlin("plugin.serialization") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
-    id("net.mamoe.mirai-console") version "2.15.0-M1"
+    id("net.mamoe.mirai-console") version "2.15.0"
 }
 
 group = "com.yulin"
@@ -22,12 +22,14 @@ dependencies {
     implementation("org.apache.commons:commons-lang3:3.12.0")
     implementation("org.jsoup:jsoup:1.15.4")
     implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("net.mamoe:mirai-core-jvm:2.15.0-M1")
-    implementation("top.jfunc.common:converter:1.8.0")
+    implementation("net.mamoe:mirai-core-jvm:2.15.0")
+    implementation("net.mamoe:mirai-console-terminal:2.15.0")
+    // https://mvnrepository.com/artifact/com.alibaba.fastjson2/fastjson2
+    implementation("com.alibaba.fastjson2:fastjson2:2.0.35")
 
 
     implementation("com.madgag:animated-gif-lib:1.4")
-    compileOnly("org.bytedeco:javacv-platform:1.5.7")
+
 //    compileOnly
     implementation(kotlin("stdlib-jdk8"))
 }
