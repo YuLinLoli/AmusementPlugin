bcb78357e62e867bab0ec815651d2543362b0887
岚雨凛 <cheng_ying@outlook.com>
2024-07-30 11:04:16 +0800

    fix:修改随机群友为真随机
    Signed-off-by: 岚雨凛 <cheng_ying@outlook.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "com.yulin"
-version = "1.2.2"
+version = "1.2.3"
 buildConfig {
     className("BuildConfig")
     packageName("com.yulin.cg")
--- src/main/kotlin/kotlinUtil/MathUtil.kt
+++ src/main/kotlin/kotlinUtil/MathUtil.kt
@@ -1,13 +1,13 @@
 package com.yulin.kotlinUtil
 
-import java.util.*
+import java.security.SecureRandom
 
 class MathUtil {
     companion object {
         //输入一个0-999的数字，判断是否在该百分比概率，如果判断成功则返回true
         fun probability(int: Int): Boolean {
 
-            val i = Random().nextInt(1000)
+            val i = SecureRandom().nextInt(1000)
             return when {
                 i < int -> {
                     true
